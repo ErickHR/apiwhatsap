a1949c930bb1a9c6ae7869a3b0800ab89e89a0d3
Erickhr <Erick08_999@hotmail.com>
2020-12-22 22:14:11 -0600

    api4

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ function apiWhatsapp(_this){
     let msg = `Mi nombre es ${_this.elements[0].value}. `
     msg += `    Mi numero es ${_this.elements[1].value} `
     msg += `    Detalle ${_this.elements[2].value}  `
-    msg += `    Envio dede: ${_this.elements[3].value}  &NewLine;`
-    msg += `    Hasta: ${_this.elements[4].value}  &NewLine;`
+    msg += `    Envio desde: ${_this.elements[3].value} `
+    msg += `    Hasta: ${_this.elements[4].value} `
     window.open(`https://api.whatsapp.com/send?phone=51955052235&text=${msg}`, '_blank')
     
 }
